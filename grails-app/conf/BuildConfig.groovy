e553c6e18e99d2d4aaaa10c8a1e6cfe82f7a0fef
amvi <vitali.amann@dvinci.de>
2022-02-10 13:43:31 +0100

    [NEW] adjusted forked project to our requirements, updated grails to 2.5.6, adjusted target jdk from 7 to 8, added everything necessary to be able to deploy this project (MAVE-371)

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,12 +1,27 @@
-if (System.getenv('TRAVIS_BRANCH')) {
-    grails.project.repos.grailsCentral.username = System.getenv("GRAILS_CENTRAL_USERNAME")
-    grails.project.repos.grailsCentral.password = System.getenv("GRAILS_CENTRAL_PASSWORD")
-}
+//if (System.getenv('TRAVIS_BRANCH')) {
+//    grails.project.repos.grailsCentral.username = System.getenv("GRAILS_CENTRAL_USERNAME")
+//    grails.project.repos.grailsCentral.password = System.getenv("GRAILS_CENTRAL_PASSWORD")
+//}
 
 grails.project.work.dir = 'target'
 grails.project.docs.output.dir = 'docs/manual' // for gh-pages branch
-grails.project.source.level = 1.7
-grails.project.target.level = 1.7
+grails.project.source.level = 1.8
+grails.project.target.level = 1.8
+
+// add repository information to deploy it to our artifactory
+// determine snapshot version
+boolean isSnapshot = false
+new File("${basedir}").eachFile {
+    if (it.name.endsWith("GrailsPlugin.groovy")) {
+        isSnapshot = it.readLines().any { it.contains("version") && it.contains('SNAPSHOT') }
+    }
+}
+
+//Nexus Configuration
+grails.project.repos.default = ""
+grails.project.repos.dvinci.url = ""
+grails.project.repos.dvinci.username = ""
+grails.project.repos.dvinci.password = ""
 
 grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
@@ -28,11 +43,11 @@ grails.project.dependency.resolution = {
             exclude group: 'org.springframework', name: 'spring-context-support'
             exclude group: 'org.springframework', name: 'spring-context'
         }
-
+        compile 'org.apache.maven.wagon:wagon-http:1.0-beta-2'
     }
 
     plugins {
-        build(':release:3.1.1', ':rest-client-builder:2.1.1') {
+        build(':release:3.1.3', ':rest-client-builder:2.1.1') {
             export = false
         }
         compile ':cache:1.1.8'
